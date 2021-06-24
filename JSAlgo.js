1216232b618656be3d25ff3df765fa0d7fde1773
Zach-Jeffcoat <84590361+Zach-Jeffcoat@users.noreply.github.com>
2021-06-24 13:47:34 -0500

    Add files via upload

---
+++ JSAlgo.js
@@ -0,0 +1,12 @@
+var num=1
+for(num=1; num<20; num+=2)
+    console.log(num)
+
+    var x=1
+    var sum=0
+    
+    for(x=1; x<=5; x++){
+        var sum=x+sum;
+    console.log("Num:", x)
+    console.log("Sum:", sum)
+    }
